bb41c9e8e30b3c9f9dd7c6226e3c58ed609e0ed2
Ng Manh Tien <127041010+nmtien8386@users.noreply.github.com>
2024-06-12 21:37:45 +0700

    first commit

---
+++ src/app/components/list-product/list-product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-product.component.html',
+})
+export class ListProductComponent {
+
+}
---
+++ src/app/components/update-product/update-product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-product.component.html',
+})
+export class UpdateProductComponent {
+
+}
